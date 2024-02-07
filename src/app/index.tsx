90dbbbf60ccdcdfefe478f28a40a9cb747d8ae84
Matheus Gomes <math.s.gomes@gmail.com>
2024-02-06 22:25:25 -0300

    configurações tailwind css e native wind

---
+++ src/app/index.tsx
@@ -0,0 +1,16 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Olá mundo com React Native</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'blue',
+    flex: 1
+  }
+})
