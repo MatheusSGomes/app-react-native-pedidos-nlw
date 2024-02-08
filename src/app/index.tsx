72a6522e4516c95a9b3cfdb0fef2aae2a68a52fe
Matheus Gomes <math.s.gomes@gmail.com>
2024-02-07 22:04:41 -0300

    product

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,15 @@
 import { CategoryButton } from "@/components/category-button";
 import { Header } from "@/components/header";
-import { View, FlatList } from "react-native";
-import { CATEGORIES } from '@/utils/data/products'
+import { View, FlatList, SectionList, Text } from "react-native";
+import { CATEGORIES, MENU } from "@/utils/data/products";
 import { useState } from "react";
+import { Product } from "@/components/product";
 
 export default function Home() {
   const [category, setCategory] = useState(CATEGORIES[0]);
 
   function handleCategorySelect(selectedCategory: string) {
-    setCategory(selectedCategory)
+    setCategory(selectedCategory);
   }
 
   return (
@@ -18,7 +19,7 @@ export default function Home() {
       <FlatList
         data={CATEGORIES}
         keyExtractor={(item) => item}
-        renderItem={({item}) => (
+        renderItem={({ item }) => (
           <CategoryButton
             title={item}
             isSelected={item === category}
@@ -30,6 +31,21 @@ export default function Home() {
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
       />
+
+      <SectionList
+        sections={MENU}
+        keyExtractor={(item) => item.id}
+        stickySectionHeadersEnabled={false}
+        renderItem={({ item }) => <Product data={item} />}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text className="text-xl text-white font-heading mt-8 mb-3">
+            {title}
+          </Text>
+        )}
+        className="flex-1 p-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      />
     </View>
-  )
+  );
 }
