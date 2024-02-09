086b303e4419a470cd5438bcc5c0f68dc3ee1f5f
Matheus Gomes <math.s.gomes@gmail.com>
2024-02-08 22:36:30 -0300

    input pedido e botão voltar ao cardápio

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,10 +1,17 @@
 import { View, Text, ScrollView } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
 import { useCartStore } from "@/stores/cart-store";
 
 import { Header } from "@/components/header";
-import { Product } from "@/components/product";
 import { formatCurrency } from "@/utils/functions/format-currency";
+
+import { Product } from "@/components/product";
+import { Input } from "@/components/input";
+import { Button } from "@/components/button";
+
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { LinkButton } from "@/components/link-button";
 
 export default function Cart() {
   const cartStore = useCartStore();
@@ -19,25 +26,45 @@ export default function Cart() {
   return (
     <View className="flex-1 pt-8">
       <Header title="Seu carrinho" />
-      <ScrollView>
-        {cartStore.products.length > 0 ? (
+
+      <KeyboardAwareScrollView>
+        <ScrollView>
           <View className="p-5 flex-1">
-            {cartStore.products.map((product) => (
-              <Product key={product.id} data={product} />
-            ))}
+            {cartStore.products.length > 0 ? (
+              <View className="border-b border-slate-700">
+                {cartStore.products.map((product) => (
+                  <Product key={product.id} data={product} />
+                ))}
+              </View>
+            ) : (
+              <Text className="font-body text-slate-400 text-center my-8">
+                Seu carrinho está vazio
+              </Text>
+            )}
+
+            <View className="flex-row gap-2 items-center mt-5 mb-4">
+              <Text className="text-white text-xl font-subtitle">Total: </Text>
+
+              <Text className="text-lime-400 text-2xl font-heading">
+                {total}
+              </Text>
+            </View>
+
+            <Input placeholder="Informe o endereço de entrega com rua, bairro, CEP, número e complemento..." />
           </View>
-        ) : (
-          <Text className="font-body text-slate-400 text-center my-8">
-            Seu carrinho está vazio
-          </Text>
-        )}
+        </ScrollView>
+      </KeyboardAwareScrollView>
 
-        <View className="flex-row gap-2 items-center mt-5 mb-4">
-          <Text className="text-white text-xl font-subtitle">Total: </Text>
+      <View className="p-5 gap-5">
+        <Button>
+          <Button.Text>Enviar pedido</Button.Text>
+          <Button.Icon>
+            <Feather name="arrow-right-circle" size={20} />
+          </Button.Icon>
+        </Button>
 
-          <Text className="text-lime-400 text-2xl font-heading">{total}</Text>
-        </View>
-      </ScrollView>
+        <LinkButton title="Voltar ao cardápio" href="/"></LinkButton>
+      </View>
     </View>
   );
 }
